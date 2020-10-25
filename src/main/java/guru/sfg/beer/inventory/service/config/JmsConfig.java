33829608571eb987372a75b0619e752900af4515
ozcanmelih <melih.ozcan89@gmail.com>
2020-10-25 15:47:12 +0300

    Parents updated

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package guru.sfg.beer.inventory.service.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -9,11 +10,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class JmsConfig {
 
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+
     @Bean
-    public MessageConverter messageConverter() {
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,19 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = 9164301642224106756L;
+
+    private BeerDto beerDto;
+}
